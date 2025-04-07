c1e1f085365d4610b62aafebfba259e0885c6487
Anatoly Chemulov <anatolytchemulov@yandex.ru>
2025-04-07 14:43:45 +0300

    добавил аннотации Allure к файлам page

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,42 +7,42 @@ class MainPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver, url="https://stellarburgers.nomoreparties.site/")
 
-    @allure.step("Переход к форме входа")
+    @allure.title("Переход к форме входа")
     def go_to_login(self):
         self.find_and_click(MainPageLocators.LOGIN_BUTTON)
 
-    @allure.step("Переход в личный кабинет")
+    @allure.title("Переход в личный кабинет")
     def go_to_personal_account(self):
         self.find_and_click(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)
 
-    @allure.step("Проверка открытия главной страницы")
+    @allure.title("Проверка открытия главной страницы")
     def is_main_page_opened(self):
         return self.is_element_visible(MainPageLocators.PAGE_TITLE)
 
-    @allure.step("Переключение на вкладку 'Булки'")
+    @allure.title("Переключение на вкладку 'Булки'")
     def switch_to_buns_tab(self):
         self.find_and_click(MainPageLocators.BUNS_TAB)
 
-    @allure.step("Переключение на вкладку 'Соусы'")
+    @allure.title("Переключение на вкладку 'Соусы'")
     def switch_to_sauces_tab(self):
         self.find_and_click(MainPageLocators.SAUCES_TAB)
 
-    @allure.step("Переключение на вкладку 'Начинки'")
+    @allure.title("Переключение на вкладку 'Начинки'")
     def switch_to_fillings_tab(self):
         self.find_and_click(MainPageLocators.FILLINGS_TAB)
 
-    @allure.step("Проверка активности раздела 'Булки'")
+    @allure.title("Проверка активности раздела 'Булки'")
     def is_buns_section_active(self):
         return self.is_element_visible(MainPageLocators.BUNS_SECTION)
 
-    @allure.step("Проверка активности раздела 'Соусы'")
+    @allure.title("Проверка активности раздела 'Соусы'")
     def is_sauces_section_active(self):
         return self.is_element_visible(MainPageLocators.SAUCES_SECTION)
 
-    @allure.step("Проверка активности раздела 'Начинки'")
+    @allure.title("Проверка активности раздела 'Начинки'")
     def is_fillings_section_active(self):
         return self.is_element_visible(MainPageLocators.FILLINGS_SECTION)
 
-    @allure.step("Клик по логотипу 'Stellar Burgers'")
+    @allure.title("Клик по логотипу 'Stellar Burgers'")
     def click_logo(self):
         self.find_and_click(MainPageLocators.LOGO)
--- pages/profile_page.py
+++ pages/profile_page.py
@@ -6,20 +6,20 @@ class ProfilePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver, url="https://stellarburgers.nomoreparties.site/profile")
 
-    @allure.step("Выход из аккаунта пользователя")
+    @allure.title("Выход из аккаунта пользователя")
     def logout(self):
         self.find_and_click(ProfilePageLocators.LOGOUT_BUTTON)
 
-    @allure.step("Проверка открытия страницы профиля")
+    @allure.title("Проверка открытия страницы профиля")
     def is_profile_page_opened(self):
         return self.is_element_visible(ProfilePageLocators.PROFILE_HEADER)
 
-    @allure.step("Переход на страницу конструктора")
+    @allure.title("Переход на страницу конструктора")
     def go_to_constructor(self):
         from locators.main_locators import MainPageLocators
         self.find_and_click(MainPageLocators.CONSTRUCTOR_BUTTON)
 
-    @allure.step("Клик по логотипу сайта")
+    @allure.title("Клик по логотипу сайта")
     def click_logo(self):
         from locators.main_locators import MainPageLocators
         self.find_and_click(MainPageLocators.LOGO)
